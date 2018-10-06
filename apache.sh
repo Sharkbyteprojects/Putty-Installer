159f6faea1a5d34d2f27bf76231859a1171b9db8
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2018-10-06 16:15:02 +0200

    Add files via upload

---
+++ apache.sh
@@ -0,0 +1,6 @@
+# Geben sie folgendes ein: chmod +x apache.sh
+clear
+echo Eventuell müssen sie das ROOT Kennwort eingeben. Geben sie dies ein wenn sie aufgefordert werden. Bei Fragen wegen Festplattenspeicher müssen sie J eingeben
+sudo apt-get install apache2
+clear
+echo Gehen sie nun ins Verzeichnis des Servers: cd /var/www/html/. Dateien die sie dort ablegen, werden über den webserver zugänglich.
