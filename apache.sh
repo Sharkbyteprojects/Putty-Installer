953dcc2a4e8f1a04e9b2e9e75b502f34e499f198
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2019-01-01 13:10:51 +0100

    Update apache.sh

--- apache.sh
+++ apache.sh
@@ -2,6 +2,7 @@
 #Gehört nicht zu Putty Installer!
 clear
 echo Eventuell müssen sie das ROOT Kennwort eingeben. Geben sie dies ein wenn sie aufgefordert werden. Bei Fragen wegen Festplattenspeicher müssen sie J eingeben
+sudo apt-get update
 sudo apt-get install apache2
 clear
 echo Gehen sie nun ins Verzeichnis des Servers: cd /var/www/html/. Dateien die sie dort ablegen, werden über den webserver zugänglich.
